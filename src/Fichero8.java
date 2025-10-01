31918bf367b6fcceb6f1171e5bdd87be24f49cff
RezeileT <sebastianbarrios1812@gmail.com>
2025-10-01 19:32:31 +0200

    01/10/2025 Ejercicios de creación de directorios y ficheros

---
+++ src/Fichero8.java
@@ -0,0 +1,14 @@
+public class Fichero8 {
+    public static void main(String[] args) {
+
+    }
+    public static void explorarCarpeta(){
+
+    }
+    public static void analizarElemento(){
+
+    }
+    public static void ConvertirAURI(){
+
+    }
+}
