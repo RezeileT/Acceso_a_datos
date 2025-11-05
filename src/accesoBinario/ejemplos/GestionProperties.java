a2912b27dea76c6e0bc3a346216469ed792d62c1
AlumnoAfternoon <sebastianbarrios1812@gmail.com>
2025-11-05 19:23:16 +0100

    05/11/25 - Configurando properties

--- src/accesoBinario/ejemplos/GestionProperties.java
+++ src/accesoBinario/ejemplos/GestionProperties.java
@@ -6,7 +6,7 @@ import java.util.Properties;
 public class GestionProperties {
     public static void main(String[] args) {
         Properties props = new Properties();
-        File archivo = new File("config.properties");
+        File archivo = new File("src/accesoBinario/resources/config.properties");
 
         // Intentar cargar desde archivo existente
         try (FileInputStream fis = new FileInputStream(archivo)) {
